55647bc020482369368c4df1ff49d69ae42c68fe
Jéssica Prestelo <jessica.p.jesus@aluno.senai.br>
2025-04-03 14:07:02 -0300

    feat: criação dos components e alterações na Header e Footer

---
+++ atividadeavaliativa-front-end/src/app/layout.jsx
@@ -0,0 +1,27 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Atividade Avaliativa Front End",
+  description: "Componentização com props next.js",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
